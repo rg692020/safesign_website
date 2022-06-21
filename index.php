e0045a8df756d76a3efe0333eb8e594339f02e5b
Newt42 <59628325+Newt4217@users.noreply.github.com>
2022-06-21 09:55:40 +0200

    add some translations

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php
-	$sprache=Array();
-
+	include('sprachen.php');
+	if (isset($_GET['wahl']) and ($_GET['wahl'] >= 0) and ($_GET['wahl'] <= 3))
+	{
+		$i = $_GET['wahl'];
+	}
+	else
+	{
+		$i = 3;
+	}
 
 ?>
 
@@ -16,11 +23,11 @@
 	<!-- This is the outline for the navbar, used to navigate between different parts of the website -->
 	<div class="topnav">
 		<a href="" style=background-color: green;>Home</a>
-		<a href="./Produkt/">Unser Produkt</a>
+		<a href="./Produkt/"><?php echo $sprache[$i]['Produkt']?></a>
 		<a href="./Sicherheit/">Sicherheit</a>
 		<a href="./Team/">Team</a>
-		<a href="./Kontakt/">Kontakt</a>
-		<a href="./Impressum/">Impressum</a>
+		<a href="./Kontakt/"><?php echo $sprache[$i]['Kontakt']?></a>
+		<a href="./Impressum/"><?php echo $sprache[$i]['Impressum']?></a>
 		<a style=text-align: right;>Change language</a>
 	</div>
 	<a> Hauptseite </a>
---
+++ sprachen.php
@@ -0,0 +1,30 @@
+<?php
+	$sprache=Array();
+
+	$sprache[0]['Name']='Deutsch';
+	$sprache[1]['Name']='english';
+	$sprache[2]['Name']='français';
+	$sprache[3]['Name']='español';
+
+    //Start of navbar translation
+    $sprache[0]['Produkt']='Unser Produkt';
+    $sprache[1]['Produkt']='Our product';
+    $sprache[2]['Produkt']='Notre produit';
+    $sprache[3]['Produkt']='Nuestro producto';
+
+    $sprache[0]['Kontakt']='Kontakt';
+    $sprache[1]['Kontakt']='Contact';
+    $sprache[2]['Kontakt']='Contact';
+    $sprache[3]['Kontakt']='Contacto';
+
+    $sprache[0]['Impressum']='Impressum';
+    $sprache[1]['Impressum']='imprint';
+    $sprache[2]['Impressum']='empreinte';
+    $sprache[3]['Impressum']='pie de imprenta';
+
+    $sprache[0]['Ueberschrift']='Hauptseite';
+    $sprache[1]['Ueberschrift']='Homepage';
+    $sprache[2]['Ueberschrift']='sité du main';
+    $sprache[3]['Ueberschrift']='página principal';
+
+?>
