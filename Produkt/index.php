95a6b16a0eb2dc619646660e38985e1eaf4b98d2
Newt42 <59628325+Newt4217@users.noreply.github.com>
2022-06-21 10:40:53 +0200

    add some more navbar translations

--- Produkt/index.php
+++ Produkt/index.php
@@ -1,6 +1,5 @@
 <?php
-	include('../sprachen.php');
-	$i = 3;
+	include('../sprachen.php')
 
 ?>
 
@@ -14,13 +13,13 @@
 <body>
 	<!-- This is the outline for the navbar, used to navigate between different parts of the website -->
 	<div class="topnav">
-		<a href="" style=background-color: green;>Home</a>
-		<a href="../Produkt/"><?php echo $sprache[$i]['Produkt']?></a>
+	<a href="" style=background-color: green;>Home</a>
+		<a href="../Produkt/" style=background-color:green;><?php echo $sprache[$i]['Produkt']?></a>
 		<a href="../Sicherheit/"><?php echo $sprache[$i]['Sicherheit']?></a>
-		<a href="../Team/" style=background-color:green;>Team</a>
+		<a href="../Team/">Team</a>
 		<a href="../Kontakt/"><?php echo $sprache[$i]['Kontakt']?></a>
 		<a href="../Impressum/"><?php echo $sprache[$i]['Impressum']?></a>
 		<a style=text-align: right;>Change language</a>
 	</div>
-	<a> Team </a>
+	<a> Produkt </a>
 </body>
--- Sicherheit/index.php
+++ Sicherheit/index.php
@@ -1,8 +1,8 @@
 <?php
-	include('../sprachen.php');
+	include('../sprachen.php')
 	$i = 3;
-
 ?>
+
 
 <!DOCTYPE html>
 <head>
@@ -16,11 +16,11 @@
 	<div class="topnav">
 		<a href="" style=background-color: green;>Home</a>
 		<a href="../Produkt/"><?php echo $sprache[$i]['Produkt']?></a>
-		<a href="../Sicherheit/"><?php echo $sprache[$i]['Sicherheit']?></a>
-		<a href="../Team/" style=background-color:green;>Team</a>
+		<a href="../Sicherheit/" style=background-color:green;><?php echo $sprache[$i]['Sicherheit']?></a>
+		<a href="../Team/">Team</a>
 		<a href="../Kontakt/"><?php echo $sprache[$i]['Kontakt']?></a>
 		<a href="../Impressum/"><?php echo $sprache[$i]['Impressum']?></a>
 		<a style=text-align: right;>Change language</a>
 	</div>
-	<a> Team </a>
+	<a> Sicherheit </a>
 </body>
