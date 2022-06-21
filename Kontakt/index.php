908ff39ae04e910c217e95240d503046f6821c1e
Newt42 <59628325+Newt4217@users.noreply.github.com>
2022-06-21 11:01:04 +0200

    add another translated navbar

---
+++ Kontakt/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<head>
+	<meta charset="UTF-8">
+	<title>SafeSign</title>
+	
+	<link href="style.css" type="text/css" rel="stylesheet" >
+</head>
+<body>
+	<!-- This is the outline for the navbar, used to navigate between different parts of the website -->
+	<div class="topnav">
+		<a href="../Produkt/"><?php echo $sprache[$i]['Produkt']?></a>
+		<a href="../Sicherheit/"><?php echo $sprache[$i]['Sicherheit']?></a>
+		<a href="../Team/">Team</a>
+		<a href="../Kontakt/" style=background-color:green;><?php echo $sprache[$i]['Kontakt']?></a>
+		<a href="../Impressum/"><?php echo $sprache[$i]['Impressum']?></a>
+	<a style=text-align: right;>Change language</a>	</div>
+		<a> Kontakt </a>
+</body>
